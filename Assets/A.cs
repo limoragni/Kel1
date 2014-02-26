64363921bf0896b1638dc1d8d9b25f3ffa6e493b
vdeluca <deluca.vicente@gmail.com>
2014-02-26 17:51:11 -0300

    primeras modificaciones
    probando con line

---
+++ Assets/A.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown(){
+		p = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+		
+		tab.Add(p);
+
+		Debug.Log ("click dentro de la A");
+	}
+}
