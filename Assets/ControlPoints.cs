064591270fe013937d2397e53cd96be5dcd52371
vdeluca <deluca.vicente@gmail.com>
2014-03-12 10:51:39 -0300

    puntos de control

---
+++ Assets/ControlPoints.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPoints : MonoBehaviour {
+
+	private int clickCounter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		clickCounter++;
+		if (clickCounter == 1){
+
+
+
+		}
+	}
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path : MonoBehaviour {
+
+	public GameObject startNode;
+	public GameObject endNode;
+	public bool complete;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
